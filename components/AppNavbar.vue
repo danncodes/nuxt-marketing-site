167d13b3f609f1aaf1d259074e475f8ee50264a5
Daniel <daniel@personifyxp.com>
2021-10-27 17:18:27 +0100

    Deletion of Starting Components

---
+++ components/AppNavbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav class="h-14 bg-[F6F6F6] shadow-sm">
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <Tutorial/>
+  <AppNavbar />
 </template>
 
 <script>
